860d569609aa43bbe99eee050722487628224e6b
Sophie Archer <hello@sophiearcher.co.uk>
2020-01-21 21:29:20 +0000

    routes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+50.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: "chinese"
+  )
+  restaurant.save!
+  puts 'created restaurant'
+end
+
