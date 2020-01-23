d2db8b6178f305a97862d18df36f227c7477b494
Sophie Archer <hello@sophiearcher.co.uk>
2020-01-23 21:37:25 +0000

    views added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,5 @@
 class RestaurantsController < ApplicationController
   def index
-    # a user can see a list of all restaurants
     @restaurants = Restaurant.all
   end
 
@@ -8,23 +7,30 @@ class RestaurantsController < ApplicationController
     # add a new restaurant and be redirected to the show view of that restaurant
     # GET "restaurants/new"
     # POST "restaurants"
-    @restaurants = Restaurant.new
-    redirect_to restaurants_path
+    @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaurant.new(params[:restaurant])
-    @restaurant = Restaurant.save
-    redirect_to restaurant_path
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
     # see details of a restaurant with all reviews related
-    @restaurants = Restaurant.find(params[:restaurant])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def edit
-    @restaurants = Restaurant.find(params[:restaurant])
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,23 @@
 class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @reviews = Review.new
+    @review = Review.new
   end
 
   def create
-    # add a new review for a restaurant
-    @reviews = Review.new(review_params)
+    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurants_path(@restaurant)
+    if @review.save!
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
-end
-
-# GET "restaurants/38/reviews/new"
-# POST "restaurants/38/reviews"
 
 private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
+end
